09e84fb555b5277172088a08ea4d0f2833d393f1
Samuel Stein <Samuelstein1224@gmail.com>
2019-10-13 19:26:11 -0400

    Fixed a mistake

--- globals.py
+++
@@ -1,8 +0,0 @@
-def initialize(app):
-	app.config["MAIL_SERVER"] = "smtp.gmail.com"
-	app.config["MAIL_PORT"] = 465
-	app.config["MAIL_USE_SSL"] = True
-	app.config["MAIL_USERNAME"] = 'samuelstein1224.webapp@gmail.com'
-	app.config["MAIL_PASSWORD"] = 'web_app_password_new1224'
-	app.config['MAIL_DEFAULT_SENDER'] = 'samuelstein1224.webapp@gmail.com'
-	app.secret_key = 'DEVELOPMENT_KEY_122421'
