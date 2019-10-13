32493c23d11649e87c83d3590af805fa55a57063
Samuel Stein <Samuelstein1224@gmail.com>
2019-10-13 19:13:49 -0400

    Security issue, fixed

--- globals.py
+++ globals.py
@@ -3,6 +3,6 @@ def initialize(app):
 	app.config["MAIL_PORT"] = 465
 	app.config["MAIL_USE_SSL"] = True
 	app.config["MAIL_USERNAME"] = 'samuelstein1224.webapp@gmail.com'
-	app.config["MAIL_PASSWORD"] = 'webapp_password'
+	app.config["MAIL_PASSWORD"] = 'webapp_password_new2'
 	app.config['MAIL_DEFAULT_SENDER'] = 'samuelstein1224.webapp@gmail.com'
 	app.secret_key = 'DEVELOPMENT_KEY_11'
