d08c42bdc531b4ac3ed7afcbf5d00b449feb12f2
Samuel Stein <Samuelstein1224@gmail.com>
2019-10-13 19:19:34 -0400

    fixing;

--- globals.py
+++
@@ -1,8 +0,0 @@
-def initialize(app):
-	app.config["MAIL_SERVER"] = "smtp.gmail.com"
-	app.config["MAIL_PORT"] = 465
-	app.config["MAIL_USE_SSL"] = True
-	app.config["MAIL_USERNAME"] = 'samuelstein1224.webapp@gmail.com'
-	app.config["MAIL_PASSWORD"] = 'wAebapp_password_new2'
-	app.config['MAIL_DEFAULT_SENDER'] = 'samuelstein1224.webapp@gmail.com'
-	app.secret_key = 'DEVELOPMENT_KEY_11'
